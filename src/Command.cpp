aca1623d15add6dddea338dfe16caf33a5621cf8
JBeto <jbeto001@ucr.edu>
2017-02-16 15:21:02 -0800

    updated Command.cpp

--- src/Command.cpp
+++ src/Command.cpp
@@ -1,24 +1,32 @@
 #include "Command.h"
-#include <unistd.h>
 
-Command::Command(string cmd, vector<string> args) {
-    this->cmd = cmd;
-    this->args = args;
+Command::Command(const vector<string> &v)
+{
+    args = new char*[v.size() + 1];
+    for(unsigned i = 0; i < v.size(); i++) {
+        args[i] = new char[v[i].size() + 1];
+        strcpy(args[i], v[i].c_str());
+    }
+    args[v.size()] = NULL;
 }
 
 bool Command::execute()
 {
-	pid_t pid = fork();
-	if (pid < 0)
-	{
-		perror("fork failed.");
-		exit(1);
-	}
-	else if (pid == 0)
-	{
-		char* args[] = {"./dfdfdf"}
-		execv()
-	}
-		
-	return false;
+    pid_t pid = fork();
+    
+    if (pid < 0) //failed
+    {
+        perror("Fork failed");
+        return false;
+    }
+    else if (pid == 0) //child process
+    {
+        execvp(args[0], args);
+    }
+    else //parent
+    {
+        wait(NULL);
+    }
+        
+    return true;
 }
