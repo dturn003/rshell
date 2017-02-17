f15a6f52c1aead0c23c28c11987bc0ecc0c5edcd
JBeto <jbeto001@ucr.edu>
2017-02-17 15:47:47 -0800

    bool -> int execute

--- src/AndConnector.h
+++ src/AndConnector.h
@@ -7,7 +7,7 @@ class AndConnector : public Connector
 {
 public:
 	AndConnector(BaseAction* left_temp, BaseAction* right_temp) : Connector(left_temp, right_temp) { };
-	bool execute();
+	int execute();
 };
 
 #endif
