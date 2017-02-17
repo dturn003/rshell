55ecb7ef3e90760bf2eb6e006205c32a191cd1f3
JBeto <jbeto001@ucr.edu>
2017-02-17 11:19:20 -0800

    changed the way Command takes in args

--- src/main.cpp
+++ src/main.cpp
@@ -7,113 +7,119 @@
 #include "Command.h"
 
 #include <boost/tokenizer.hpp>
+using namespace std;
+using namespace boost;
 
+
+void parse(const string &input) {
+    
+    BaseAction* root = 0;
+    BaseAction* curr = root;
+    
+    vector<string> currArguments; //vector of argument list
+    bool expectCommand = true; //flag to expect next command
+    
+    typedef tokenizer<char_separator<char > > Tok; //defines tokenizer object
+    char_separator<char> sep(" ", ";&|#"); //splits up string input into tokens of connector and commands/arguments
+    Tok tokens(input, sep); //list of tokens
+    
+     for (Tok::iterator it = tokens.begin(); it != tokens.end(); ++it) {
+        if (it->at(0) == ';') {
+            BaseAction* left = new Command(currArguments);
+            if(curr == 0) {
+                curr = new AlwaysConnector(left, 0);
+            }
+            else {
+                curr->setRight(new AlwaysConnector(left, 0));
+                curr = curr->getRight();
+            }
+            if(root == 0) {
+                root = curr;
+            }
+
+            currArguments.clear();
+            expectCommand = true;
+        }
+        else if (it->at(0) == '#') {
+            break;
+        }
+        else if (it->at(0) == '&') {
+            BaseAction* left = new Command(currArguments);
+            
+            if(curr == 0) {
+                curr = new AndConnector(left, 0);
+            }
+            else {
+                curr->setRight(new AndConnector(left, 0));
+                curr = curr->getRight();
+            }
+            if(root == 0) {
+                root = curr;
+            }
+            
+            it++;
+            currArguments.clear();
+            expectCommand = true;
+        }
+        else if (it->at(0) == '|') {
+            BaseAction* left = new Command(currArguments);
+
+            if(curr == 0) {
+                curr = new OrConnector(left, 0);
+            }
+            else {
+                curr->setRight(new OrConnector(left, 0));
+                curr = curr->getRight();
+            }
+            if(root == 0) {
+                root = curr;
+            }
+
+
+            it++;
+            currArguments.clear();
+            expectCommand = true;
+        }
+        else {
+            if (expectCommand) {
+                expectCommand = false;
+                currArguments.clear();
+                currArguments.push_back(*it);
+            }
+            else {
+                currArguments.push_back(*it);
+            }
+        }
+    }
+    
+    if(!expectCommand) {
+        if(curr == 0) {
+            curr = new Command(currArguments);
+        }
+        else {
+            curr->setRight(new Command(currArguments));
+        }
+        if (root == 0) {
+            root = curr;
+        }
+    }
+    if(root) {
+        root->execute();
+    }
+}
 
 int main() {
-	bool exit = false;
-	while(!exit) {
-		std::cout << "$ ";
-		std::string input;
-		std::getline(std::cin, input);
-		
-                //temporary
-                if(input == "exit") {
-                    exit = true;
-                }
-
-		std::string currCommand;
-		std::vector<std::string> currArguments;
-                
-                BaseAction* root = 0;
-        
-                boost::char_separator<char> sep(" ", ";&|#");
-                boost::tokenizer<boost::char_separator<char > > tokens(input, sep);
-                bool expectCommand = true;
-                BaseAction* curr = root;
-                
-                for (boost::tokenizer<boost::char_separator<char > >::iterator it = tokens.begin(); it != tokens.end(); ++it) {
-                    if (it->at(0) == ';') {
-                        BaseAction* left = new Command(currCommand, currArguments);
-                        if(curr == 0) {
-                            curr = new AlwaysConnector(left, 0);
-                        }
-                        else {
-                            curr->setRight(new AlwaysConnector(left, 0));
-                            curr = curr->getRight();
-                        }
-                        if(root == 0) {
-                            root = curr;
-                        }
-                        currCommand.clear();
-                        currArguments.clear();
-                        expectCommand = true;
-                    }
-                    else if (it->at(0) == '#') {
-                        break;
-                    }
-                    else if (it->at(0) == '&') {
-                        BaseAction* left = new Command(currCommand, currArguments);
-                        
-                        if(curr == 0) {
-                            curr = new AndConnector(left, 0);
-                        }
-                        else {
-                            curr->setRight(new AndConnector(left, 0));
-                            curr = curr->getRight();
-                        }
-                        if(root == 0) {
-                            root = curr;
-                        }
-                        it++;
-                        currCommand.clear();
-                        currArguments.clear();
-                        expectCommand = true;
-                    }
-                    else if (it->at(0) == '|') {
-                        BaseAction* left = new Command(currCommand, currArguments);
- 
-                        if(curr == 0) {
-                            curr = new OrConnector(left, 0);
-                        }
-                        else {
-                            curr->setRight(new OrConnector(left, 0));
-                            curr = curr->getRight();
-                        }
-                        if(root == 0) {
-                            root = curr;
-                        }
-
-
-                        it++;
-                        currCommand.clear();
-                        currArguments.clear();
-                        expectCommand = true;
-                    }
-                    else {
-                        if (expectCommand) {
-                            expectCommand = false;
-                            currCommand = *it;
-                            currArguments.clear();
-                        }
-                        else {
-                            currArguments.push_back(*it);
-                        }
-                    }
-                }
-                if(!expectCommand) {
-                    if(curr == 0) {
-                        curr = new Command(currCommand, currArguments);
-                    }
-                    else {
-                        curr->setRight(new Command(currCommand, currArguments));
-                    }
-                    if (root == 0) {
-                        root = curr;
-                    }
-                }
-                if(root) {
-                    root->execute();
-                }
-	}
-	return 0;
-}
+    
+    while(1) {
+        cout << "$ ";
+        string input;
+        getline(cin, input);
+    
+        if (input == "exit")
+            return 0;
+            
+        parse(input);
+    }
+   
+    return 0;
+}df
