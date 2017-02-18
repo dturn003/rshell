1894ee86b2b5af7c2dbadb38289aed203b459581
JBeto <jbeto001@ucr.edu>
2017-02-17 21:56:32 -0800

    Returned to original

--- src/main.cpp
+++ src/main.cpp
@@ -5,151 +5,122 @@
 #include "AlwaysConnector.h"
 #include "OrConnector.h"
 #include "Command.h"
-#include <stack>
+
 #include <boost/tokenizer.hpp>
+#include <unistd.h>
+
 using namespace std;
 using namespace boost;
 
-//the stack represents the left and right nodes that need to be set into prevConnector
-//resetStack sets these connections and returns false if there is an error in tree construction
-bool resetStack(Connector* connect, stack<BaseAction*> &op)
-{
-    if (op.empty())
-    {
-        return false; //stack was empty, return false for bad user input
-    }
-    else
-    {
-        connect->setRight(op.top()); //sets right child of the connector
-        op.pop();
-        
-        if (op.empty())
-        {
-            return false; //stack was empty after popping only 1 element, bad user input
-        }
-        else
-        {
-            connect->setLeft(op.top());
-            op.pop();
-            op.push(connect); //pushes next node to build on tree
-            return true;
-        }
-    }
-}
 
-bool parse(const string &input) //returns false if exiting, otherwise true
-{
-    Connector* prevConnector = 0; //keeps track of the previous connector
-    stack<BaseAction*> operands; //operations to be connected with, always max 2 in the stack
-  
-    vector<string> args; //vector of argument list
+bool parse(const string &input) {
+    
+    BaseAction* root = 0;
+    BaseAction* curr = root;
+    
+    vector<string> currArguments; //vector of argument list
+    bool expectCommand = true; //flag to expect next command
     
     typedef tokenizer<char_separator<char > > Tok; //defines tokenizer object
     char_separator<char> sep(" ", ";&|#"); //splits up string input into tokens of connector and commands/arguments
     Tok tokens(input, sep); //list of tokens
     
-    for (Tok::iterator it = tokens.begin(); it != tokens.end(); ++it) //goes through list of tokens
-    {
-        if (it->at(0) == '#') //stop parsing, it's a comment
+     for (Tok::iterator it = tokens.begin(); it != tokens.end(); ++it) {
+        if (it->at(0) == ';') {
+            BaseAction* temp = new Command(currArguments);
+            if(curr == 0) {
+                curr = new AlwaysConnector(temp, 0);
+                root = curr;
+            }
+            else {
+                if(curr->getLeft() == 0) {
+                    curr->setLeft(temp);
+                }
+                else if (curr->getRight() == 0) {
+                    curr->setRight(temp);
+                }
+                root = new AlwaysConnector(curr, 0);
+                curr = root;
+            }
+            currArguments.clear();
+            expectCommand = true;
+        }
+        else if (it->at(0) == '#') {
             break;
+        }
+        else if (it->at(0) == '&') {
+            BaseAction* temp = new Command(currArguments);
             
-        if (it->at(0) == ';') //checks if AlwaysConnector
-        {
-            if (args.size() != 0)
-            {
-                operands.push(new Command(args));
-                args.clear();
+            if(curr == 0) {
+                curr = new AndConnector(temp, 0);
+                root = curr;
+            }
+            else {
+                if(curr->getLeft() == 0) {
+                    curr->setLeft(temp);
+                }
+                else if (curr->getRight() == 0) {
+                    curr->setRight(temp);
+                }
+                root = new AndConnector(curr, 0);
+                curr = root;
             }
             
-            if (prevConnector)
-            {
-                bool syntax = resetStack(prevConnector, operands);
-                if (!syntax) //user input is incorrect
-                {
-                    cout << "syntax error" << endl;
-                    return true;
+            it++;
+            currArguments.clear();
+            expectCommand = true;
+        }
+        else if (it->at(0) == '|') {
+            BaseAction* temp = new Command(currArguments);
+
+            if(curr == 0) {
+                curr = new OrConnector(temp, 0);
+                root = curr;
+            }
+            else {
+                if(curr->getLeft() == 0) {
+                    curr->setLeft(temp);
                 }
+                else if (curr->getRight() == 0) {
+                    curr->setRight(temp);
+                }
+                root = new OrConnector(curr, 0);
+                curr = root;
             }
 
-            prevConnector = new AlwaysConnector(0, 0); //replace previous connect with OrConnector
-        }
-        
-        else if (it->at(0) == '&') //checks if AndConnector
-        {
-            Tok::iterator original = it;
-            if ((++it) != tokens.end() && it->at(0) == '&') //checks if next character is also &
-            {
-                if (args.size() != 0)
-                {
-                    operands.push(new Command(args));
-                    args.clear();
-                }
-                
-                if (prevConnector)
-                {
-                    bool syntax = resetStack(prevConnector, operands);
-                    if (!syntax) //user input is incorrect
-                    {
-                        cout << "syntax error" << endl;
-                        return true;
-                    }
-                }
 
-                prevConnector = new AndConnector(0, 0); //replace previous connect with OrConnector
+            it++;
+            currArguments.clear();
+            expectCommand = true;
+        }
+        else {
+            if (expectCommand) {
+                expectCommand = false;
+                currArguments.clear();
+                currArguments.push_back(*it);
             }
-            it = original;
-        }
-        
-        else if (it -> at(0) == '|') //checks if OrConnector
-        {
-            Tok::iterator original = it;
-            if ((++it) != tokens.end() && it->at(0) == '|')
-            {
-                if (args.size() != 0)
-                {
-                    operands.push(new Command(args));
-                    args.clear();
-                }
-                
-                if (prevConnector)
-                {
-                    bool syntax = resetStack(prevConnector, operands);
-                    if (!syntax) //user input is incorrect
-                    {
-                        cout << "syntax error" << endl;
-                        return true;
-                    }
-                }
-    
-                prevConnector = new OrConnector(0, 0); //replace previous connect with OrConnector
+            else {
+                currArguments.push_back(*it);
             }
-            it = original;
-        }
-        
-        else //Else, add to args
-        {
-            args.push_back(*it);
         }
     }
     
-    if (args.size() != 0)
-        operands.push(new Command(args)); //pushes back last command
-    
-    if (prevConnector) //connects last connector
-    {
-        bool syntax = resetStack(prevConnector, operands);
-        if (!syntax) //user input is incorrect
-        {
-            cout << "syntax error" << endl;
-            return true;
+    if(!expectCommand) {
+        if(curr == 0) {
+            curr = new Command(currArguments);
+        }
+        else {
+            curr->setRight(new Command(currArguments));
+        }
+        if (root == 0) {
+            root = curr;
         }
     }
-    
-    if (!operands.empty() && operands.top()->execute() == -1)
-    {
-        return false;
+    if(root) {
+        if(root->execute() == -1) {
+            return false;
+        }
     }
-    
     return true;
 }
 
