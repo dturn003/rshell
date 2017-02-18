f0c0747f809cd708318ee85caa6db5cca75a6abc
Damian Turner <dturn003@hammer.cs.ucr.edu>
2017-02-17 22:13:06 -0800

    updated exit test

--- tests/exit.sh
+++ tests/exit.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-../bin/rshell < exit_tests
+../bin/rshell < exit_test1
+../bin/rhsell < exit_test2
+../bin/rshell < exit_test3
+../bin/rshell < exit_test4
+../bin/rshell < exit_test5
+../bin/rshell < exit_test6
