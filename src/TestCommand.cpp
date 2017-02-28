40512de1ea8fda6cb3c55b76dfff7deaf590d004
dturn003 <dturn003@ucr.edu>
2017-02-28 02:48:07 +0000

    possibly finished testcommand functionality

--- src/TestCommand.cpp
+++ src/TestCommand.cpp
@@ -8,20 +8,44 @@ int TestCommand::execute() {
     if (length != 4) {
         cout << "Improper input: test command" << endl;
     }
-    struct stat buf;
-    if(stat(args[2], &buf) < 0) {
+    
+    struct stat buf; //struct returned by stat
+    int statOut = stat(args[2], &buf);
+    if (statOut < 0) {
         perror("stat() failed");
     }
+
+    
     if (flag == 'e') {
-        
+        if (statOut == 0) {
+            cout << "(False)" << endl;
+            return 0;
+        }
+        else {
+            cout << "(True)" << endl;
+            return 1;
+        }
     }
     else if (flag == 'f') {
-        
+        if (statOut == 0) {
+            cout << "(False)" << endl;
+            return 0;
+        }
+        else {
+            return S_ISREG(buf.st_mode);
+        }
     }
     else if (flag == 'd') {
-        
+        if (statOut == 0) {
+            cout << "(False)" << endl;
+            return 0;
+        }
+        else {
+            return S_ISDIR(buf.st_mode);
+        }
     }
     else {
         cout << "Improper flag: test command" << endl;
+        return -1;
     }
 }
