c0364a8f965a3dffe0d5833116c548960ccdb7d5
jbeto <jbeto001@ucr.edu>
2017-03-05 07:19:34 +0000

    updated with smart pointers

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,325 +1,173 @@
 #include "Parser.h"
-#include "BracketTestCommand.h"
-#include "WordTestCommand.h"
 
-
-//helper function for process
-//creates the connectors in the heap and returns their pointers
-//argument op must be pre-determiend to be And, Or, Always before calling this function.
-//left and right should be pre-determiend to be valid as well
-Connector* Parser::createConnect(Operator op, BaseAction* left, BaseAction* right)
-{
-    switch(op)
-    {
-        case And:
-            return new AndConnector(left, right);
-            break;
-        case Or:
-            return new OrConnector(left, right);
-            break;
-        case Always:
-            return new AlwaysConnector(left, right);
-            break;
-        default: //default, should never arrive here
-            return 0;
-            break;
-    }
-}
-
-//deletes the current tree
-void Parser::deleteTree(BaseAction* curr)
-{
-    if (!curr) {
-        return;
-    }
-        
-    BaseAction* left = curr->getLeft();
-    BaseAction* right = curr->getRight();
-    
-    if (left)
-    {
-        deleteTree(left);
-    }
-    if (right)
-    {
-        deleteTree(right);
-    }
-    delete curr;
-}
-
-//helper function for parse
-//creates new command based on given args vector and pushes it onto operands stack
-//clears args vector
-void Parser::pushCommand(stack<BaseAction*> &operands, vector<string> &args)
-{
-    if (args.size() != 0) //checks if empty command
-    {
-        if (args.at(0) == "exit")
-        {
-            operands.push(new ExitCommand());
+void Parser::pushCommand(std::stack<std::unique_pointer<BaseAction> > &operands, std::vector<std::string> &args) {
+    if (args.size() != 0) { //checks if empty command 
+        if (args.at(0) == "exit") {
+            operands.push(std::make_unique<ExitCommand>(args));
         }
-        else if (args.at(0) == "test")
-        {
-            operands.push(new WordTestCommand(args));
+        else if (args.at(0) == "test") {
+            operands.push(std::make_unique<TestCommand>(args));
         }
-        else if (args.at(0) == "[" && args.at(args.size() - 1) == "]")
-        {
-            operands.push(new BracketTestCommand(args));
+        else if (args.at(0) == "[") {
+            operands.push(std::make_unique<TestCommand>(args));
         }
-        else
-        {
-             operands.push(new Command(args));
+        else {
+             operands.push(std::make_unique<BinCommand>(args));
         }
         args.clear();
     }
 }
 
-//helper function for cycle
-//process() pops the top Operator in connects, two operands are popped, and the resultant connector is pushed into operands
-//process returns true if user input is correct, and false if the connector is missing a command to form its left and right pointers
-//process assumes connects.top() is a valid Operator i.e. And, Or, Always NOT Left
-bool Parser::process(stack<Operator> &connects, stack<BaseAction*> &operands)
-{
-    if (operands.empty())
-    {
-        return false; //stack was empty, return false for bad user input
+bool Parser::process(std::stack<std::unique_pointer<BaseAction> > &connects, std::stack<std::unique_pointer<BaseAction> > &operands) {
+    if (operands.empty()) {
+        return false; //std::stack was empty, Connector has no children. process failed.
     }
-    else
-    {
-        BaseAction* rightOp = operands.top();
+    else {
+        std::unique_pointer<BaseAction> rightOp = operands.top();
         operands.pop();
         
-        if (operands.empty())
-        {
-            deleteTree(rightOp);
-            return false; //stack was empty after popping only 1 element, bad user input
+        if (operands.empty()) {
+            return false; //std::stack was empty after popping only 1 element, Connector does not have 2 children. process failed.
         }
-        else
-        {
-            BaseAction* leftOp = operands.top();
+        else {
+            std::unique_pointer<BaseAction> leftOp = operands.top();
             operands.pop();
-            operands.push(createConnect(connects.top(), leftOp, rightOp)); //creates the connector and pushes it to op
+            std::unique_pointer<BaseAction> result = connects.top();
+            result->setLeft(leftOp);
+            result->setRight(rightOp);
+            operands.push(result); 
             connects.pop();
-            return true;
+            return true; //process succeeded.
         }
     }
 }
 
-//cycle keeps processing the connects stack until the connects stack is empty or hits a left parenthesis
-//cycle returns true if user input is correct, and false if the connector is missing a command to form its left and right pointers
-bool Parser::cycle(stack<Operator> &connects, stack<BaseAction*> &operands)
-{
-    while (!connects.empty() && (connects.top() != Left)) //keep processing until we hit a left parenthesis or empty operator stack
-    {
-        bool syntax = process(connects, operands); //syntax represents if the user input is correct or not
-        if (!syntax)
-        {
+//change first 'if' to 'while' if there is precedence among connectors
+bool Parser::cycle(std::stack<std::unique_pointer<BaseAction> > &connects, std::stack<std::unique_pointer<BaseAction> > &operands) {
+    if (!connects.empty() && (connects.top() != LEFT)) { //keep processing until we hit a left parenthesis or empty std::unique_pointer<BaseAction> in the connect stack
+        bool syntax = process(connects, operands); //syntax represents if each Connector has the correct number of children to process.
+        if (!syntax) {
             return false;
         }
     }
     return true;
 }
 
-//checks if char delimiter link appears twice in a row
-//if yes, return true, otherwise false
-//assumes iterator it starts as valid i.e. not == to last or beyond last
 bool Parser::checkDouble(char link, Tok::iterator &it, Tok::iterator last)
 {
-    if (it == last) //if it is already last
-    {
+    if (it == last) { //if iterator is already last token. No double to be read in.
         return false;
     }
     
-    if ( ( (++it) != last ) && it->empty() ) //checks if next character is an empty token
-    {
-        if ( ( (++it) != last ) && it->at(0) == link ) //checks if same character
-        {
+    if ( ( (++it) != last ) && it->empty() ) { //checks if next character is an empty token
+        if ( ( (++it) != last ) && it->at(0) == link ) { //checks if same character
             return true;
         }
-        else
-        {
+        else {
             return false;
         }
     }
-    else
-    {
+    else {
         return false;
     }
 }
 
-//takes in string input and tokenizes it.
-//constructs and executes the design hierarchy if input is valid
-//if input is not valid, returns true to cycle back for more user input, else exit
-//true = continue to get user input, false = exit
-//utilizes infix evaluation algorithm to determine parsing
-bool Parser::parse(const string &input) //returns false if exiting, otherwise true
+std::unique_pointer<BaseAction> Parser::parse(const std::string &input)
 {
-    stack<Operator> connects;
-    stack<BaseAction*> operands; //operations to be connected with, always max 2 in the stack
-    vector<string> args; //vector of argument list
+    //initializes two std::stacks for Djikstra's shunting yard algorithm
+    std::stack<std::unique_pointer<BaseAction> > connects; //Connectors
+    std::stack<std::unique_pointer<BaseAction> > operands; //Commands/Connectors
     
-    boost::char_separator<char> sep(" ", ";&|#()", boost::keep_empty_tokens); //splits up string input into tokens of connector and commands/arguments
+    std::vector<std::string> args; //stores std::vector of arguments for reading in commands
+    
+    boost::char_separator<char> sep(" ", ";&|#()", boost::keep_empty_tokens); //splits up std::string input into tokens of connector and commands/arguments
     Tok tokens(input, sep); //list of tokens
     Tok::iterator last = tokens.end(); //ending token
     
-    for (Tok::iterator it = tokens.begin(); it != last; ++it) //goes through list of tokens
-    {
-        if ((*it) != "") //if not blank token
-        {
+    for (Tok::iterator it = tokens.begin(); it != last; ++it) { //iterates through list of tokens
+        if ((*it) != "") { //if not blank token
             char link = it->at(0);
         
             if (link == '#') //stop parsing, it's a comment
                 break; //exits for loop
-            else if (link == '(')
-            {
-                if (args.size() != 0) //there should be no command to push to operands, if there are, there is a user input error
-                {
-                    if (!operands.empty())
-                        deleteTree(operands.top());
-                    return true;
+            else if (link == '(') {
+                if (args.size() != 0) { //there should be no command to push to operands, if there are, there is a user input error
+                    cout << "Unexpected token near '('" << std::endl;
+                    return nullptr;
                 }
-                
-                connects.push(Left); //push left parenthesis
+                connects.push(LEFT); //push left parenthesis
             }
-            else if (link == ')')
-            {
-                pushCommand(operands, args); //clears args, pushes new command onto operands stack
-    
-                if (!cycle(connects, operands))
-                {
-                    if (!operands.empty())
-                        deleteTree(operands.top());
-                    return true;
+            else if (link == ')') {
+                pushCommand(operands, args);
+                if (!connects.empty() && connects.top() == LEFT) {
+                    cout << "Unexpected token near ')'" << std:: endl;
+                    return nullptr;
                 }
-                if (connects.empty()) //there should still be a left parenthesis if correct
-                {
-                    if (!operands.empty())
-                        deleteTree(operands.top());
-                    return true; //user input error: no left parenthesis
+                if (!cycle(connects, operands)) {
+                    //TODO: unaware of what to print here
+                    cout << "Unexpected token before ')'"
+                    return nullptr;
                 }
-                
+                if (connects.empty()) { //there should still be a left parenthesis if correct
+                    cout << "No corresponding '(' token found" << std::endl;
+                    return nullptr;
+                }
                 connects.pop(); //pops left parenthesis
             }
-            else if (link == ';') //checks if AlwaysConnector
-            {
-                pushCommand(operands, args); //clears args, pushes new command onto operands stack
-                
-                if (!cycle(connects, operands))
-                {
-                    if (!operands.empty())
-                        deleteTree(operands.top());
-                    return true;
+            else if (link == ';') { //checks if AlwaysConnector
+                pushCommand(operands, args);
+                if (!cycle(connects, operands)) {
+                    cout << "Unexpected token near ';'" << std::endl;
+                    return 0;
                 }
-                connects.push(Always);
+                connects.push(std::unique_pointer<AlwaysConnector>());
             }
-            else if (link == '&') //checks if AndConnector
-            {
-                if (checkDouble('&', it, last))
-                {
-                    pushCommand(operands, args); //clears args, pushes new command onto operands stack
-    
-                    if (!cycle(connects, operands))
-                    {
-                        if (!operands.empty())
-                            deleteTree(operands.top());
-                        return true;
+            else if (link == '&') { //initial check for checkDouble
+                if (checkDouble('&', it, last)) { //checks if AndConnector
+                    pushCommand(operands, args); 
+                    if (!cycle(connects, operands)) {
+                        cout << "Unexpected token near '&&'" << std::endl;
+                        return nullptr;
                     }
-                    connects.push(And); //pushes AndConnector onto connects
+                    connects.push(std::unique_pointer<AndConnector>()); //pushes AndConnector onto connects
                 }
-                else
-                {
-                    if (!operands.empty())
-                        deleteTree(operands.top());
-                    return true;
+                else { //detected only a single '&'
+                    cout << "Unexpected token near '&'" << std::endl;
+                    return nullptr;
                 }
-                
             }
-            else if (link == '|') //checks if OrConnector
-            {
-                if (checkDouble('|', it, last))
-                {
-                    pushCommand(operands, args); //clears args, pushes new command onto operands stack
-    
-                    if (!cycle(connects, operands))
-                    {
-                        if (!operands.empty())
-                            deleteTree(operands.top());
-                        return true;
+            else if (link == '|') { //initial check for checkDouble
+                if (checkDouble('|', it, last)) { //checks if OrConnector
+                    pushCommand(operands, args);
+                    if (!cycle(connects, operands)) {
+                        cout << "Unexpected token near '||'" << std:: endl;
+                        return nullptr;
                     }
-                    connects.push(Or); //pushes AndConnector onto connects
+                    connects.push(std::unique_pointer<OrConnector>()); //pushes OrConnector onto connects
                 }
-                else
-                {
-                    if (!operands.empty())
-                        deleteTree(operands.top());
-                    return true;
+                else { //detected only a single '|'
+                    cout << "Unexpected token near '|'" << std:: endl;
+                    return nullptr;
                 }
             }
-            else //Else, add to args vector
-            {
+            else { //Else, add to args
                 args.push_back(*it);
             }
         }
     }
     
-    pushCommand(operands, args); //clears args, pushes new command onto operands stack
-    
-    if (!cycle(connects, operands)) //cycle 
-    {
-        if (!operands.empty())
-            deleteTree(operands.top());
-        return true;
+    //Last pass to read in final command, and process final Connector if they exist.
+    pushCommand(operands, args); //pushes last command if there was one
+    if (!cycle(connects, operands)) { //cycle last connector if there is one since there was no future connector to call cycle.
+        return nullptr;
     }
-    
-    if (!connects.empty()) //remaining left parenthesis, no right parenthesis in user input
-    {
-        if (!operands.empty())
-            deleteTree(operands.top());
-        return true; 
+    else if (!connects.empty()) { //remaining left parenthesis, no right parenthesis in user input
+        return nullptr;
     }
-    
-    if (operands.empty()) //empty commands
-    {
-        return true;
+    else if (operands.empty()) { //empty command linr
+        return nullptr;
     }
-    
-    if (operands.top()->execute() == -1) //execute, if exit, return false
-    {
-        deleteTree(operands.top());
-        return false;
+    else {
+        return operands.top();
     }
-
-    deleteTree(operands.top());
-    return true;
 }
-
-
-
-/*
-//helper function for cycle
-//skeleton code, should be added to if operators/connectors have precedence
-//returns true if the precedence of link1 > link2
-//returns false if the precedence of link1 <= link2
-bool Parser::checkPrecedence(Operator link1, Operator link2)
-{
-    return false;
-}
-*/
-
-/*
-//this version of cycle accounts for precedence in its operators
-//keeps processing until connects is empty, hit a left parenthesis, or curr is of greater precedence than the previous one
-bool Parser::cycle(Operator curr)
-{
-    //keep processing until left parenthesis or empty operator stack or curr > top in precedence
-    while ( (!connects.empty() && (connects.top() != Left)) && checkPrecedence(connects.top(), curr) ) 
-    {
-        bool syntax = process(); //syntax represents if the user input is correct or not
-        if (!syntax)
-        {
-            return false;
-        }
-    }
-    return true;
-}
-*/
--- src/Parser.h
+++ src/Parser.h
@@ -1,43 +1,125 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-#include "BaseAction.h"
+//Leaves/Commands
 #include "ExitCommand.h"
 #include "TestCommand.h"
-#include "Command.h"
-#include "Connector.h"
+#include "BinCommand.h"
+
+//Composites/Connectors
 #include "AndConnector.h"
 #include "OrConnector.h"
 #include "AlwaysConnector.h"
 
-#include <vector>
+//Libraries
 #include <string>
+#include <iostream>
+#include <vector>
 #include <stack>
 #include <boost/tokenizer.hpp>
-using namespace std;
+#include <memory>
 
 class Parser
 {
-    typedef boost::tokenizer<boost::char_separator<char > > Tok; //defines tokenizer object
-    
     private:
-        enum Operator {And, Or, Always, Left}; //Left = (, Always = ;, Or = ||, And = &&
+        typedef boost::tokenizer<boost::char_separator<char > > Tok; //defines tokenizer object
+        std::unique_pointer<BaseAction> LEFT = nullptr; //represents Left parenthesis, means of identifying '(' in Connector stack
         
-        //bool checkPrecedence(Operator link1, Operator link2); //checks precedence
-        //bool cycle(Operator curr);
+        /*
+            Assumptions: The type of command is determined by args.at(0), or the command name.
+                         If the command fits the format of any of our built in commands, we create
+                         that command. If not, it will automatically allocate that command as a bin 
+                         command. It is up to the individual command classes to determine if the 
+                         actual command was correct or not when execute() is called.
+                         
+            Description: pushCommand takes the complete command and argument list, std::vector<std::string> &args,
+                         and determines what command to push to the std::stack of operands. If there is no 
+                         command to push(args is empty), then the method does not push the command into
+                         the operands std::stack. pushCommand then clears args so the parser may read in the 
+                         next command.
+                         
+                    Use: Use pushCommand when the parser detects that the complete args list is finished.
+                         This situation occurs during one of two scenarios
+                         1.     We have read in a connector (&&, ||, ;)
+                         2.     We have reached the end of the token list (Last command)
+        */
+        void pushCommand(std::stack<std::unique_pointer<BaseAction> > &operands, std::vector<std::string> &args);
         
-        void deleteTree(BaseAction* curr);
+        /*
+            Assumptions: process assumes that it is perfectly fine to start connecting the top Connector
+                         of the connects std::stack with its children. The other method, cycle, takes care of 
+                         when to process. This means the connect stack must not be empty and top != nullptr.
+                         process also assumes connector is binary (only has two children).
+                         
+            Description: process returns 'true' if the current Connector has been correctly processed.
+                         process is defined by the following steps:
+                         1.     Pop operand std::stack twice. Each std::unique_pointer<BaseAction> element represents
+                                the left or right action (connectors or commands) to be connected
+                                with the the current top Connector of the connects std::stack.
+                                
+                         2.     Pop the connects std::stack once. Now that the top Connector is being
+                                parsed, we no longer needs to store it as a connector to
+                                be processed in the future.
+                         
+                         3.     Push resultant Connector onto operands std::stack. The resultant connector
+                                is defined as having been connected with its correct children/operands.
+                                We push the resultant connector onto the operands std::stack so that it can be
+                                used as the child of the next connector we may parse in the future.
+                                
+                    Use: Use process when we have determined that another Connector has been read in from
+                         the token list, and it is fine to process previous connectors.
+        */
+        bool process(std::stack<std::unique_pointer<BaseAction> > &connects, std::stack<std::unique_pointer<BaseAction> > &operands);
+        
+        /*
+            Assumptions: connects stack is not empty; there is a top std::unique_pointer<BaseAction>.
+                         
+            Description: cycle correctly parses the syntax tree in the correct order in which
+                         each Connector has come. We need to process the previous Connectors to correctly 
+                         parse the tree for the next Connector to be saved in the connects std::stack and for
+                         future Connectors to have the correct children according to the syntax of the inputs.
+                         
+                    Use: Use cycle for when another Connector has been read in from the token list. If
+                         precedence is not equal among Connectors in future add-ons, then another method to
+                         check precedence must be added and applied to the while loop when the parser reads
+                         in normal Connectors, such as ;, &&, ||. However, this specific case of cycle does 
+                         not apply when the parser reads in a ')'. In the case that precedence is unequal among 
+                         Connectors and the parser has read in a ')', the while loop should ignore the precedence check.
+        */
+        bool cycle(std::stack<std::unique_pointer<BaseAction> > &connects, std::stack<std::unique_pointer<BaseAction> > &operands);
+        
+        /*
+            Assumptions: checkDouble assumes that an &, | has been read in from inputs and needs to be checked
+                         if it is immediately followed by a corresponding &, |, resulting in a complete
+                         AndConnector (&&) or OrConnector(||).
+                         
+            Description: checkDouble checks if user input is a complete && or ||. checkDouble moves the current 
+                         (it) position to the next token to be correctly read in (after second & or |).
+                         This is to prevent the parser from double reading the second corresponding &, | as
+                         a seperate token and call checkDouble again erroneously. If there is a correct double
+                         && or ||, then checkDouble returns true. Else, false.
+                         
+                    Use: Use checkDouble when a Connector to be read in requires two of the same character twice
+                         in a row. i.e. &&, ||.
+        */
         bool checkDouble(char link, Tok::iterator &it, Tok::iterator last);
-        bool process(stack<Operator> &connects, stack<BaseAction*> &operands); //helper function for cycle
-        bool cycle(stack<Operator> &connects, stack<BaseAction*> &operands);
         
-        Connector* createConnect(Operator op, BaseAction* left, BaseAction* right); //helper function for process
-        void pushCommand(stack<BaseAction*> &operands, vector<string> &args); //helper function for parse
-
     public:
-        
         Parser() {}; //default constructor
-        bool parse(const string &input); //returns false if exiting, otherwise true
+        
+        /*
+            Assumptions: parse assumes that the complete tree can be constructed from one line of user input.
+                         parse does not take in multiple lines of user input.
+                         
+            Description: parse splits up the input into readable tokens, and applies a version of
+                         Djikstra's shunting yard algorithm where commands are operands and 
+                         Connectors( &&, ;, ||) are operations. parse returns a pointer to the completed
+                         syntax tree if user input was correct or returns a nullptr and prints an error
+                         message otherwise.
+                         
+                    Use: Use parse when the user had input a string of commands in the shell.
+        */
+        bool parse(const std::string &input);
 };
 
 #endif
