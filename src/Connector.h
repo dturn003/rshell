718823cbae89458377e40342bf3fb017897d9764
jbeto <jbeto001@ucr.edu>
2017-03-10 20:41:29 +0000

    Added destructor

--- src/Connector.h
+++ src/Connector.h
@@ -8,21 +8,25 @@ class Connector : public BaseAction {
         BaseAction* left;
         BaseAction* right;
     public:
-        Connector(BaseAction* left_temp, BaseAction* right_temp) : left(left_temp), right(right_temp) {}
-
+        Connector(BaseAction* left_temp, BaseAction* right_temp) : left(left_temp), right(right_temp) {};
+        ~Connector() {
+            delete left;
+            delete right;
+        };
+        
         virtual int execute() = 0;
         BaseAction* getLeft() {
             return left;
-        }
+        };
         BaseAction* getRight() {
             return right;
-        }
+        };
         void setLeft(BaseAction* left) {
             this->left = left;
-        }
+        };
         void setRight(BaseAction* right) {
             this->right = right;
-        }
+        };
 };
 
 #endif
