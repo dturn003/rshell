438f112f11c422be47957968ec8ec0d8f03b942b
dturn003 <dturn003@ucr.edu>
2017-03-14 23:18:11 +0000

    created pipeconnector files

---
+++ src/PipeConnector.h
@@ -0,0 +1,14 @@
+#ifndef PIPCONNECTOR_H
+#define PIPECONNECTOR_H
+
+#include "BaseAction.h"
+
+class PipeConnector : public Connector {
+    private:
+        
+    public:
+        PipeConnector();
+        int execute();
+};
+
+#endif
