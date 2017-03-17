d47b839fb2cbf78daa396a4d730596b1c0e8563a
dturn003 <dturn003@ucr.edu>
2017-03-17 03:36:59 +0000

    Pipe works somewhat correctly now, need to fix infinite loop probably by adding wait

--- src/PipeConnector.cpp
+++ src/PipeConnector.cpp
@@ -22,17 +22,23 @@ int PipeConnector::execute() {
         return 0;
     }
     else if (pid == 0) { //child process, writes - left child
+        close(1);
         close(pipefd[0]);
         // dup(pipefd[1]);
         dup2(pipefd[1], fileno(stdout));
+        close(pipefd[1]);
         this->left->execute();
+        
         return 1;
     }
     else { //parent process, reads - right child
+        close(0);
         close(pipefd[1]);
         // dup(pipefd[0]); // puts read end of pipe in dup
         dup2(pipefd[0], fileno(stdin));
+        close(pipefd[0]);
         this->right->execute();
+        
         return 1;
     }
     
