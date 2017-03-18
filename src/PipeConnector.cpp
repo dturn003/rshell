f5d3543b9086d690c51e378d9d8e67ada0e6bf0c
dturn003 <dturn003@ucr.edu>
2017-03-18 01:57:05 +0000

    pipe connector works correctly 100%

--- src/PipeConnector.cpp
+++ src/PipeConnector.cpp
@@ -1,6 +1,7 @@
 #include "PipeConnector.h"
 
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <sys/wait.h>
 
@@ -22,42 +23,44 @@ int PipeConnector::execute() {
         return 0;
     }
     else if (pid1 == 0) { //child process, writes - left child
-        close(1);
         close(pipefd[0]);
-        // dup(pipefd[1]);
-        dup2(pipefd[1], fileno(stdout));
+        close(1);
+        
+        dup(pipefd[1]);
+        // dup2(pipefd[1], fileno(stdout));
         close(pipefd[1]);
+        
         this->left->execute();
         
-        return 1;
+        exit(1);
     }
-    else { //parent process, reads - right child
         
-        pid_t pid2 = fork();
-        if (pid2 < 0) { //fork failed, return 0
-            perror("Call fork failed");
-            return 0;
-        }
-        else if (pid2 == 0) { //2nd child process, writes - left child
-            close(0);
-            close(pipefd[1]);
-            // dup(pipefd[0]); // puts read end of pipe in dup
-            dup2(pipefd[0], fileno(stdin));
-            close(pipefd[0]);
-            this->right->execute();
-            
-            return 1;
-        }
-        
-        //ensure pipe is closed
-        close(pipefd[0]);
+    pid_t pid2 = fork();
+    if (pid2 < 0) { //fork failed, return 0
+        perror("Call fork failed");
+        return 0;
+    }
+    else if (pid2 == 0) { //2nd child process, writes - left child
         close(pipefd[1]);
-        //wait for both children to finish
-        wait(0);
-        wait(0);
+        close(0);
         
-        return 1;
+        dup(pipefd[0]); // puts read end of pipe in dup
+        // dup2(pipefd[0], fileno(stdin));
+        close(pipefd[0]);
+        
+        this->right->execute();
+        
+        exit(1);
     }
+    
+    //ensure pipe is closed so children can finish
+    close(pipefd[0]);
+    close(pipefd[1]);
+    //wait for both children to finish
+    wait(0);
+    wait(0);
+    
+    return 1;
     
     
     
