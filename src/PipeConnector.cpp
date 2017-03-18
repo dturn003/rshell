3d48b9100c9fc61baf4aa46e3556f8509e2b555a
dturn003 <dturn003@ucr.edu>
2017-03-18 01:28:09 +0000

    more fixes to pipeconnector, almost works but still affects the next commands input

--- src/PipeConnector.cpp
+++ src/PipeConnector.cpp
@@ -15,13 +15,13 @@ int PipeConnector::execute() {
         return 0;
     }
     
-    pid_t pid = fork();
+    pid_t pid1 = fork();
     
-    if (pid < 0) { //fork failed, return 0
+    if (pid1 < 0) { //fork failed, return 0
         perror("Call fork failed");
         return 0;
     }
-    else if (pid == 0) { //child process, writes - left child
+    else if (pid1 == 0) { //child process, writes - left child
         close(1);
         close(pipefd[0]);
         // dup(pipefd[1]);
@@ -32,15 +32,33 @@ int PipeConnector::execute() {
         return 1;
     }
     else { //parent process, reads - right child
-        close(0);
-        close(pipefd[1]);
-        // dup(pipefd[0]); // puts read end of pipe in dup
-        dup2(pipefd[0], fileno(stdin));
+        
+        pid_t pid2 = fork();
+        if (pid2 < 0) { //fork failed, return 0
+            perror("Call fork failed");
+            return 0;
+        }
+        else if (pid2 == 0) { //2nd child process, writes - left child
+            close(0);
+            close(pipefd[1]);
+            // dup(pipefd[0]); // puts read end of pipe in dup
+            dup2(pipefd[0], fileno(stdin));
+            close(pipefd[0]);
+            this->right->execute();
+            
+            return 1;
+        }
+        
+        //ensure pipe is closed
         close(pipefd[0]);
-        this->right->execute();
+        close(pipefd[1]);
+        //wait for both children to finish
+        wait(0);
+        wait(0);
         
         return 1;
     }
+    
     
     
 }
